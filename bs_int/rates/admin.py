f1a40b27a6aedc248e856561b2417a106543eeae
Kevin Yokley <kyokley2@gmail.com>
2024-04-03 10:01:13 -0500

    Add DataSet to Django Admin

--- bs_int/rates/admin.py
+++ bs_int/rates/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from bs_int.rates.models import DataSet
+
 # Register your models here.
+@admin.register(DataSet)
+class DataSetAdmin(admin.ModelAdmin):
+    ordering = ('-date',)
