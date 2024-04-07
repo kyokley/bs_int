8cd9a984d780832836345f900c035040808586e2
Kevin Yokley <kyokley2@gmail.com>
2024-04-07 00:24:36 -0500

    Differentiate MEDIA_ROOT and MEDIA_URL

--- bs_int/site/prod_settings.py
+++ bs_int/site/prod_settings.py
@@ -3,5 +3,5 @@ from .settings import *  # noqa
 ALLOWED_HOSTS = ['bsint', 'comet.dyndns.info']
 CSRF_TRUSTED_ORIGINS = ['https://comet.dyndns.info']
 
-MEDIA_ROOT = '/static'
+MEDIA_ROOT = '/static-assets'
 MEDIA_URL = '/static/'
