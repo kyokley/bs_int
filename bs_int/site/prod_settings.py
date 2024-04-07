b71196475a54dac561df76a046a01703934fe2ef
Kevin Yokley <kyokley2@gmail.com>
2024-04-07 00:14:06 -0500

    Add MEDIA settings to prod config

--- bs_int/site/prod_settings.py
+++ bs_int/site/prod_settings.py
@@ -2,3 +2,6 @@ from .settings import *  # noqa
 
 ALLOWED_HOSTS = ['bsint', 'comet.dyndns.info']
 CSRF_TRUSTED_ORIGINS = ['https://comet.dyndns.info']
+
+MEDIA_ROOT = '/static'
+MEDIA_URL = '/static'
