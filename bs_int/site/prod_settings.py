64fc8b03a3e9e7cdc892b55ed66fd9f3c1bc4636
Kevin Yokley <kyokley2@gmail.com>
2024-04-07 00:58:46 -0500

    Update MEDIA_ROOT

--- bs_int/site/prod_settings.py
+++ bs_int/site/prod_settings.py
@@ -3,5 +3,5 @@ from .settings import *  # noqa
 ALLOWED_HOSTS = ['bsint', 'comet.dyndns.info']
 CSRF_TRUSTED_ORIGINS = ['https://comet.dyndns.info']
 
-MEDIA_ROOT = '/static-assets'
+MEDIA_ROOT = '/images-dir'
 MEDIA_URL = '/images/'
