17067626ec6d2234bab4a6c8db3ea986073278a3
Kevin Yokley <kyokley2@gmail.com>
2024-04-07 19:03:39 -0500

    Change default password

--- bs_int/rates/management/commands/initdata.py
+++ bs_int/rates/management/commands/initdata.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import Permission
 from django.core.management.base import BaseCommand
 
 logger = logging.getLogger(__file__)
-WERT66 = "wert66"
+PASS = "zero yield curve"
 
 ADMIN = "admin"
 USER = "user"
@@ -21,10 +21,10 @@ class Command(BaseCommand):
         User = get_user_model()
 
         if not User.objects.filter(username=ADMIN).exists():
-            User.objects.create_superuser(ADMIN, password=WERT66)
+            User.objects.create_superuser(ADMIN, password=PASS)
 
         if not User.objects.filter(username=USER).exists():
-            user = User.objects.create_user(USER, password=WERT66, is_staff=True)
+            user = User.objects.create_user(USER, password=PASS, is_staff=True)
 
             for class_str in ("treasurydata",):
                 user.user_permissions.add(
